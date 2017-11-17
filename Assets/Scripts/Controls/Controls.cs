f827e60108be0095dd1915d723dc76ad0e9e4748
Pierre CAMILLI <camillipierre@gmail.com>
2017-11-17 23:12:22 +0100

    Ajoute l'interface de contrôles

---
+++ Assets/Scripts/Controls/Controls.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : SingletonBehaviour<Controls> {
+
+    [SerializeField]
+    private PlayerController[] _playerControls;
+
+    public PlayerController Player(byte player = 0)
+    {
+        return _playerControls[player];
+    }
+}
+
+[System.Serializable]
+public class PlayerController
+{
+#region Unity Axis
+    [SerializeField]
+    private string m_leftButton = "LeftButton";
+
+    [SerializeField]
+    private string m_rightButton = "RightButton";
+    #endregion
+
+    public float Horizontal
+    {
+        get { return Input.GetAxisRaw(m_rightButton) - Input.GetAxisRaw(m_leftButton); }
+    }
+
+    public bool SwapDown
+    {
+        get { return Input.GetButtonDown(m_rightButton) && Input.GetButtonDown(m_leftButton); }
+    }
+
+    public bool Swap
+    {
+        get { return Horizontal == 0f; }
+    }
+    
+    public bool SwapUp
+    {
+        get { return Input.GetButtonUp(m_rightButton) && Input.GetButtonUp(m_leftButton); }
+    }
+}

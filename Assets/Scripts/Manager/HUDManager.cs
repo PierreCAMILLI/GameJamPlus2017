0562a3564452377d82527fffdc1452f82dd56711
Pierre CAMILLI <camillipierre@gmail.com>
2017-11-19 00:37:47 +0100

    Ajoute le HUD au jeu.

---
+++ Assets/Scripts/Manager/HUDManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : SingletonBehaviour<HUDManager> {
+
+    [Header("Texts")]
+    [SerializeField]
+    Text _timer;
+
+    [SerializeField]
+    Text _firstPlayerScore;
+    [SerializeField]
+    Text _secondPlayerScore;
+
+    // Update is called once per frame
+    void Update () {
+        if (_timer != null)
+            _timer.text = Mathf.FloorToInt(GameManager.Instance.Timer).ToString();
+        if(_firstPlayerScore != null)
+            _firstPlayerScore.text = Mathf.FloorToInt(GameManager.Instance.PlayerStats[0].FallenObjects).ToString();
+        if (_secondPlayerScore != null)
+            _secondPlayerScore.text = Mathf.FloorToInt(GameManager.Instance.PlayerStats[1].FallenObjects).ToString();
+    }
+}

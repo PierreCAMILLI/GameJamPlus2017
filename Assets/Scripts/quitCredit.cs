4352b99531d0aec46b91a3f2e0f6c83bd204d6ce
mcerutti <marc.cerutti@etu.u-bordeaux.fr>
2017-11-19 19:36:47 +0100

    SceneCredit

---
+++ Assets/Scripts/quitCredit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitCredit : MonoBehaviour {
+
+	IEnumerator couroutine;
+	public float delay;
+	// Use this for initialization
+	void Start () {
+
+		couroutine = quitGame(delay);
+		StartCoroutine(coroutine);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	IEnumerator quitGame(float delay)
+	{
+		_playcorout = true;
+		yield return new WaitForSecondsRealtime(5f);
+		Application.Quit();
+		
+	}
+}
